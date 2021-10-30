225679e1ff68a8bd2e758aeb18be657de71fb418
Zlatko S <zstapic@foi.hr>
2021-10-30 10:05:08 +0200

    [Simple data view] ArrayAdapter as a framework  - How to design a List View  Adapter  - What are the similarities with framework design  - Dependancy Inversion Principle

--- app/src/main/java/hr/foi/air/dl/MainActivity.kt
+++ app/src/main/java/hr/foi/air/dl/MainActivity.kt
@@ -3,7 +3,9 @@ package hr.foi.air.dl
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
+import hr.foi.air.database.data.DataRepository
 import hr.foi.air.database.data.MockData
 import hr.foi.air.dl.databinding.ActivityMainBinding
 
@@ -11,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mockSomeData()
         //setContentView(R.layout.activity_main)
         var binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
@@ -19,11 +20,21 @@ class MainActivity : AppCompatActivity() {
 
         //val btnShowData = findViewById<Button>(R.id.btn_show_data)
         binding.btnShowData.setOnClickListener{
+            displayData(binding)
         }
     }
 
-    fun mockSomeData()
+    fun displayData(binding: ActivityMainBinding)
     {
+        //Unos podataka u bazu, ako je potrebno
         MockData.mockData(this)
+
+        var discounts : List<String>? = DataRepository(this).getDiscountNames()
+
+        //Prikaz podataka na zaslovnu
+        if (discounts != null) {
+            binding.lstDiscounts.adapter =
+                ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, discounts)
+        }
     }
 }
