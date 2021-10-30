37bef6c1d1b091fab9257314674c68c46806f70c
Zlatko S <zstapic@foi.hr>
2021-10-30 10:17:43 +0200

    [Simple data view] Additional display options  - working with toast messages  - using log and logcat  - house cleening

--- app/src/main/java/hr/foi/air/dl/MainActivity.kt
+++ app/src/main/java/hr/foi/air/dl/MainActivity.kt
@@ -2,9 +2,12 @@ package hr.foi.air.dl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.view.isVisible
 import hr.foi.air.database.data.DataRepository
 import hr.foi.air.database.data.MockData
 import hr.foi.air.dl.databinding.ActivityMainBinding
@@ -36,5 +39,18 @@ class MainActivity : AppCompatActivity() {
             binding.lstDiscounts.adapter =
                 ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, discounts)
         }
+
+        //Dodatne mogućnosti prikaza
+        for (disc in discounts!!) {
+            //privremeni ispis na zaslon
+            Toast.makeText(this, disc, Toast.LENGTH_LONG).show()
+
+            //ispis u log
+            Log.d("AIR", disc)
+        }
+
+        //hiding empty message
+        if (!discounts.isEmpty())
+            binding.emptyMessage.isVisible = false
     }
 }
