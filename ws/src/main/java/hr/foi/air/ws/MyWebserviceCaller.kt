74b573b5ff9d33501b156a9aeab3ce1151dd8cb7
Zlatko S <zstapic@foi.hr>
2021-11-13 13:12:06 +0100

    [Webservices] Initialize webservice caller  - create a new class  - define and initialize retrofit object  - define and initialize a method to obtain data about stores from web service

---
+++ ws/src/main/java/hr/foi/air/ws/MyWebserviceCaller.kt
@@ -0,0 +1,31 @@
+package hr.foi.air.ws
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.squareup.okhttp.OkHttpClient
+import hr.foi.air.ws.responses.MyWebserviceResponse
+import retrofit.*
+
+class MyWebserviceCaller {
+    var retrofit: Retrofit? = null
+    val baseUrl: String = "http://cortex.foi.hr/mtl/courses/air/"
+
+    constructor() {
+        val client: OkHttpClient = OkHttpClient()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    fun getAllStores(method: String)
+    {
+        val serviceCaller: MyWebservice? = retrofit?.create(MyWebservice::class.java)
+        var call: Call<MyWebserviceResponse>? = null
+        if (serviceCaller != null) {
+            call = serviceCaller.getAllStores(method)
+        }
+    }
+}
