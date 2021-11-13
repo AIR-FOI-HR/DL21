b98e6b0b1dfbdbfbbc6e2128c931067bc99478ac
Zlatko S <zstapic@foi.hr>
2021-11-13 13:37:19 +0100

    [Webservices] Implement a method to obtain discounts  - implement method by using the example from getAllStores  - notice the difference in json extraction as in this example the json contains a complex data type: date

--- ws/src/main/java/hr/foi/air/ws/MyWebserviceCaller.kt
+++ ws/src/main/java/hr/foi/air/ws/MyWebserviceCaller.kt
@@ -3,6 +3,7 @@ package hr.foi.air.ws
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.squareup.okhttp.OkHttpClient
+import hr.foi.air.database.entities.Discount
 import hr.foi.air.database.entities.Store
 import hr.foi.air.ws.handlers.MyWebserviceHandler
 import hr.foi.air.ws.responses.MyWebserviceResponse
@@ -50,6 +51,47 @@ class MyWebserviceCaller {
                                 if (storeItems != null) {
                                     //data obtained stend it to handler
                                     dataArrivedHandler.onDataArrived<Store>(storeItems.toList(), true, response.body().timeStamp)
+                                }
+                            }
+                        }
+                    }catch (ex: Exception){
+                        ex.printStackTrace()
+                    }
+                }
+            })
+        }
+    }
+
+    fun getAllDiscount(method: String, dataArrivedHandler: MyWebserviceHandler)
+    {
+        val serviceCaller: MyWebservice? = retrofit?.create(MyWebservice::class.java)
+        var call: Call<MyWebserviceResponse>? = null
+        if (serviceCaller != null) {
+            call = serviceCaller.getAllDiscounts(method)
+        }
+
+        if(call != null){
+            call.enqueue(object: Callback<MyWebserviceResponse> {
+                override fun onFailure(t: Throwable?) {
+                    t?.printStackTrace()
+                }
+
+                override fun onResponse(
+                    response: Response<MyWebserviceResponse>?,
+                    retrofit: Retrofit?
+                ) {
+                    try{
+                        if (response != null) {
+                            if(response.isSuccess()){
+                                println("Got discounts... Processing...")
+                                val gson : Gson = GsonBuilder()
+                                    .setDateFormat("yyyy-MM-dd")
+                                    .create()
+                                val discountItems: Array<Discount>? = gson.fromJson(response.body().items, Array<Discount>::class.java)
+
+                                if (discountItems != null) {
+                                    //data obtained stend it to handler
+                                    dataArrivedHandler.onDataArrived<Discount>(discountItems.toList(), true, response.body().timeStamp)
                                 }
                             }
                         }
