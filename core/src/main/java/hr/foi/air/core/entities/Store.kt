6ca7f66ac5d4719938cb336eed936256f7f51413
Al0cam <benjaminsikac@gmail.com>
2021-11-17 12:13:49 +0100

    [entity-fix] making core/entities/store open and the variable which will be overriden aswell

--- core/src/main/java/hr/foi/air/core/entities/Store.kt
+++ core/src/main/java/hr/foi/air/core/entities/Store.kt
@@ -1,7 +1,7 @@
 package hr.foi.air.core.entities
 
-data class Store(
-    var id : Int? = null,
+open class Store(
+    open var id : Int? = null,
     var name : String = "",
     var description : String = "",
     var imgUrl: String = "",
