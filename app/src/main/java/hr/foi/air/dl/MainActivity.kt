65dcd1d021391c3ac4267c6c62f63f55cc4b48a7
Zlatko S <zstapic@foi.hr>
2021-11-19 20:14:46 +0100

    [Modular data sources] Finalize DataRepository changes  - data repository class is a logical connection between upper architectural layers (view and viewmodel) and lower layers (data sources)  - challenge: before making these changes try to connect the new data acquisition method to existing simple list view and adapter  - upon completing the task, start reshaping data repository to fit new needs -> to prepare data on stores and discounts to be presented in expandable recycler view  - the existing method on obtaining the data on discount names was reshaped to server the purpose of obtaining all data  - the asynchronous data acquisition from modules requires the asynchronous data transfer to caller  - the interface (protocol) to send data to callers is created (LoadDataListener), and is similar to existing interface to receive data from modules  - main activity is cleaned from code that was used to display demo data from the database

--- app/src/main/java/hr/foi/air/dl/MainActivity.kt
+++ app/src/main/java/hr/foi/air/dl/MainActivity.kt
@@ -2,14 +2,9 @@ package hr.foi.air.dl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.core.view.isVisible
-import hr.foi.air.database.data.DbDataSource
-import hr.foi.air.database.data.MockData
 import hr.foi.air.dl.databinding.ActivityMainBinding
-import hr.foi.air.dl.repositry.DataRepository
+import hr.foi.air.dl.repository.DataRepository
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +15,7 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        //val btnShowData = findViewById<Button>(R.id.btn_show_data)
-        binding.btnShowData.setOnClickListener{
-            displayData(binding)
-        }
+        displayData(binding)
     }
 
 
@@ -31,25 +23,5 @@ class MainActivity : AppCompatActivity() {
     {
         var repository = DataRepository()
 
-        var discounts : List<String>? = repository.getDiscountNames(this)
-
-        //Prikaz podataka na zaslovnu
-        if (discounts != null) {
-            binding.lstDiscounts.adapter =
-                ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, discounts)
-
-            //Dodatne mogućnosti prikaza
-            for (disc in discounts!!) {
-                //privremeni ispis na zaslon
-                Toast.makeText(this, disc, Toast.LENGTH_LONG).show()
-
-                //ispis u log
-                Log.d("AIR", disc)
-            }
-
-            //hiding empty message
-            if (!discounts.isEmpty())
-                binding.emptyMessage.isVisible = false
-        }
     }
 }
