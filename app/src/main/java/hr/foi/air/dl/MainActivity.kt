201cb2bbe9df8129d9d78f25f12fede2c1089ba3
Zlatko S <zstapic@foi.hr>
2021-11-19 21:56:47 +0100

    [Expandable RecyclerView] Display data in MainActivity  - obtain the data from the repository  - convert the data from the list of stores to the list of parent stores  - create an instance of previously created adapter and connect it to the recycler view  - specify linear layout manager for recycler view  - hide "empty data" message if data arrived

--- app/src/main/java/hr/foi/air/dl/MainActivity.kt
+++ app/src/main/java/hr/foi/air/dl/MainActivity.kt
@@ -3,8 +3,14 @@ package hr.foi.air.dl
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import hr.foi.air.core.entities.Discount
+import hr.foi.air.core.entities.Store
 import hr.foi.air.dl.databinding.ActivityMainBinding
+import hr.foi.air.dl.recyclerview.StoreParent
+import hr.foi.air.dl.recyclerview.StoreRecyclerAdapter
 import hr.foi.air.dl.repository.DataRepository
+import hr.foi.air.dl.repository.LoadDataListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +28,24 @@ class MainActivity : AppCompatActivity() {
     fun displayData(binding: ActivityMainBinding)
     {
         var repository = DataRepository()
+        var context = this
 
+        repository.loadData(this, object : LoadDataListener {
+            override fun onDataLoaded(stores: List<Store>?, discounts: List<Discount>?) {
+                if (stores != null) {
+                    val parentList : ArrayList<StoreParent> = ArrayList()
+                    for (s in stores)
+                        parentList.add(StoreParent(s, discounts!!))
+                    
+                    //prikaz podataka
+                    binding.mainRecycler.adapter = StoreRecyclerAdapter(context, parentList)
+                    binding.mainRecycler.layoutManager = LinearLayoutManager(context)
+
+                    //hiding empty message
+                    if (!stores.isEmpty())
+                        binding.emptyMessage.isVisible = false
+                }
+            }
+        } )
     }
 }
