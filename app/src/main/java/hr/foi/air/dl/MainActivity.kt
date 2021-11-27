1aad6086e60c398fbffef413112f8db0d7c32e3a
Zlatko S <zstapic@foi.hr>
2021-11-27 13:07:19 +0100

    [Fragments] Displaying fragments  - change activity_main.xml to add FrameLayout which is used to display fragments. More than one FrameLayout could be placed in single activity.  - create kotlin klass holding the view. Make sure to extend base Fragment.  - override onCreateView to display your view. Observe how view binding is implemented in fragments.  - override onViewCreated to use view in rest of the code  - please read about fragment's lifecycle here: https://developer.android.com/guide/fragments/lifecycle  - please read about fragment's lifecycle relationship with activity's lifecycle. Think of benefits if one would implement custom lifecycle observer: https://developer.android.com/topic/libraries/architecture/lifecycle  - change MainActivity.kt to add code that will display fragment. Please read about the options to transfer data to fragments at the bottom of this page: https://developer.android.com/guide/fragments/create

--- app/src/main/java/hr/foi/air/dl/MainActivity.kt
+++ app/src/main/java/hr/foi/air/dl/MainActivity.kt
@@ -7,21 +7,34 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import hr.foi.air.core.entities.Discount
 import hr.foi.air.core.entities.Store
 import hr.foi.air.dl.databinding.ActivityMainBinding
+import hr.foi.air.dl.fragments.ListViewFragment
 import hr.foi.air.dl.recyclerview.StoreParent
 import hr.foi.air.dl.recyclerview.StoreRecyclerAdapter
 import hr.foi.air.dl.repository.DataRepository
 import hr.foi.air.dl.repository.LoadDataListener
 
 class MainActivity : AppCompatActivity() {
+    private var currentFragment : ListViewFragment? = null
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //setContentView(R.layout.activity_main)
-        var binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        showMainFragment()
         //displayData(binding)
+    }
+
+    private fun showMainFragment()
+    {
+        currentFragment = ListViewFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(binding.mainFragment.id, currentFragment!!)
+            .commit()
     }
 
 
